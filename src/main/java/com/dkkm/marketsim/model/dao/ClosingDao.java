5c56e9b209f038078e052834c60feccdb31e56ee
Morgan Taylor <morganxntaylor@gmail.com>
2021-06-08 12:05:58 -0700

    add dto specific methods to their respective daos

--- src/main/java/com/dkkm/marketsim/model/dao/ClosingDao.java
+++ src/main/java/com/dkkm/marketsim/model/dao/ClosingDao.java
@@ -1,6 +1,12 @@
 package com.dkkm.marketsim.model.dao;
 
 import com.dkkm.marketsim.model.dto.Closing;
+import java.time.LocalDate;
+import java.util.List;
 
 public interface ClosingDao extends CrudDao<Closing, Closing> {
+
+    List<Closing> getClosingsByDate(LocalDate date);
+    List<Closing> getClosingsByTicker(String ticker);
+    // TODO: List<Closing> getClosingsByTickerInDateRange(String ticker, LocalDate start, LocalDate stop);
 }
--- src/main/java/com/dkkm/marketsim/model/dao/StockDao.java
+++ src/main/java/com/dkkm/marketsim/model/dao/StockDao.java
@@ -2,5 +2,10 @@ package com.dkkm.marketsim.model.dao;
 
 import com.dkkm.marketsim.model.dto.Stock;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface StockDao extends CrudDao<Stock, String> {
+
+    List<Stock> getStocksByIpo(LocalDate date);
 }
