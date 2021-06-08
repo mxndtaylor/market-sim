68e38908b812731d372ed6a03fc06e04cb93f63a
Morgan Taylor <morganxntaylor@gmail.com>
2021-06-08 12:14:53 -0700

    add holdingdbdao

---
+++ src/main/java/com/dkkm/marketsim/model/dao/HoldingDBDao.java
@@ -0,0 +1,113 @@
+package com.dkkm.marketsim.model.dao;
+
+import com.dkkm.marketsim.model.dto.Holding;
+import org.hibernate.bytecode.spi.NotInstrumentedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@Repository
+public class HoldingDBDao implements HoldingDao {
+
+    @Autowired
+    private JdbcTemplate jdbc;
+
+    @Override
+    public Holding addMember(Holding holding) {
+        final String ADD_MEMBER = "INSERT INTO Holdings (PortfolioId, Ticker, PurchaseDate, Quantity) " +
+                "VALUES (?,?,?,?);";
+        int rowsAffected = jdbc.update(ADD_MEMBER,
+                holding.getPortfolioId(),
+                holding.getTicker(),
+                holding.getPurchaseDate(),
+                holding.getQuantity());
+
+        if (rowsAffected == 0) {
+            // TODO: add error throwing/catching
+        }
+
+        return holding;
+    }
+
+    @Override
+    public Holding getMemberByKey(Holding keys) {
+        throw new NotInstrumentedException("This member is not accessible by Id");
+    }
+
+    @Override
+    public List<Holding> getPortfolioHoldings(int portfolioId) {
+        List<Holding> holdingsByPortfolio; // TODO: add error catching
+
+        final String GET_MEMBER_BY_PORTFOLIO = "SELECT * FROM Holdings WHERE PortfolioId = ?;";
+        holdingsByPortfolio = jdbc.query(GET_MEMBER_BY_PORTFOLIO, new HoldingMapper(), portfolioId);
+
+        return holdingsByPortfolio;
+    }
+
+    @Override
+    public List<Holding> getPortfolioHoldingsByTicker(int portfolioId, String ticker) {
+        List<Holding> tickerHoldingsByPortfolio; // TODO: add error catching
+
+        final String GET_MEMBER_BY_PORTFOLIO_TICKER = "SELECT * FROM Holdings WHERE PortfolioId = ?, Ticker = ?;";
+        tickerHoldingsByPortfolio = jdbc.query(GET_MEMBER_BY_PORTFOLIO_TICKER, new HoldingMapper(), portfolioId, ticker);
+
+        return tickerHoldingsByPortfolio;
+
+    }
+
+    @Override
+    public List<Holding> getMembers() {
+        final String GET_ALL_MEMBERS = "SELECT * FROM Holdings;";
+        List<Holding> holdings; // TODO: add error catching
+
+        holdings = jdbc.query(GET_ALL_MEMBERS, new HoldingMapper());
+
+        return holdings;
+    }
+
+    @Override
+    public boolean deleteMemberByKey(Holding keys) {
+        final String DELETE_MEMBER = "DELETE * FROM Holdings WHERE " +
+                "PortfolioId = ?, Ticker = ?, PurchaseDate = ?;";
+        int rowsAffected = jdbc.update(DELETE_MEMBER,
+                keys.getPortfolioId(),
+                keys.getTicker(),
+                keys.getPurchaseDate());
+        return rowsAffected == 1;
+    }
+
+    @Override
+    public boolean updateMember(Holding holding) {
+        final String UPDATE_MEMBER = "UPDATE Holdings SET Quantity = ? " +
+                "WHERE PortfolioId = ?, Ticker = ?, PurchaseDate = ?;";
+        int rowsAffected = jdbc.update(UPDATE_MEMBER,
+                holding.getQuantity(),
+                holding.getPortfolioId(),
+                holding.getTicker(),
+                holding.getPurchaseDate());
+
+        return rowsAffected == 1;
+    }
+
+    private static final class HoldingMapper implements RowMapper<Holding> {
+
+        @Override
+        public Holding mapRow(ResultSet resultSet, int i) throws SQLException {
+            Holding holding = new Holding();
+
+            holding.setPortfolioId(resultSet.getInt("PortfolioId"));
+            holding.setTicker(resultSet.getString("Ticker"));
+            holding.setPurchaseDate(resultSet.getDate("PurchaseDate").toLocalDate());
+            holding.setQuantity(resultSet.getInt("Quantity"));
+
+            return holding;
+        }
+    }
+
+}
