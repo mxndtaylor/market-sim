b03c0513844372d1d7eeb227e3522e99ad434c0e
Morgan Taylor <morganxntaylor@gmail.com>
2021-06-08 13:10:02 -0700

    add PortfolioService interface

---
+++ src/main/java/com/dkkm/marketsim/service/PortfolioService.java
@@ -0,0 +1,14 @@
+package com.dkkm.marketsim.service;
+
+import com.dkkm.marketsim.model.dto.Portfolio;
+
+import java.util.List;
+
+public interface PortfolioService {
+
+    Portfolio addPortfolio(Portfolio portfolio);
+    Portfolio getPortfolioById(int id);
+    List<Portfolio> getPortfolios();
+    boolean deletePortfolioById(int id);
+    boolean updatePortfolio(Portfolio portfolio);
+}
