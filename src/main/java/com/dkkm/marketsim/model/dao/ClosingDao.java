49363979a20714630089411c2f44a86945d75fef
Morgan Taylor <morganxntaylor@gmail.com>
2021-06-08 12:46:42 -0700

    change closingsbyticker to stretch goal

--- src/main/java/com/dkkm/marketsim/model/dao/ClosingDao.java
+++ src/main/java/com/dkkm/marketsim/model/dao/ClosingDao.java
@@ -5,8 +5,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface ClosingDao extends CrudDao<Closing, Closing> {
-
     List<Closing> getClosingsByDate(LocalDate date);
-    List<Closing> getClosingsByTicker(String ticker);
+    // TODO: List<Closing> getClosingsByTicker(String ticker);
     // TODO: List<Closing> getClosingsByTickerInDateRange(String ticker, LocalDate start, LocalDate stop);
 }
