a7e23f8e7e41ee280719750775674f698f2f8d59
Morgan Taylor <morganxntaylor@gmail.com>
2021-06-08 11:03:18 -0700

    add holding dto

---
+++ src/main/java/com/dkkm/marketsim/model/dto/Holding.java
@@ -0,0 +1,43 @@
+package com.dkkm.marketsim.model.dto;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Holding {
+
+    @NotNull
+    private Stock stock;
+    @NotNull
+    private Integer quantity;
+
+    public Stock getStock() {
+        return stock;
+    }
+
+    public void setStock(Stock stock) {
+        this.stock = stock;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Holding holding = (Holding) o;
+        return stock.equals(holding.stock) && quantity.equals(holding.quantity);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(stock, quantity);
+    }
+}
