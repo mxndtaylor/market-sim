6b429381294a8996a561b961a9b232888b3d5c04
Morgan Taylor <morganxntaylor@gmail.com>
2021-06-08 08:23:11 -0700

    add spring initializr generated content

---
+++ src/main/java/com/dkkm/marketsim/MarketsimApplication.java
@@ -0,0 +1,13 @@
+package com.dkkm.marketsim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketsimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketsimApplication.class, args);
+	}
+
+}
