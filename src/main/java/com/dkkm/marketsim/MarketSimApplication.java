316a06d931868ee7c832172fb87c1db106fd1208
Morgan Taylor <morganxntaylor@gmail.com>
2021-06-08 13:52:36 -0700

    rename MarketSimApplication to have camel casing

--- src/main/java/com/dkkm/marketsim/MarketSimApplication.java
+++ src/main/java/com/dkkm/marketsim/MarketSimApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MarketsimApplication {
+public class MarketSimApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MarketsimApplication.class, args);
+		SpringApplication.run(MarketSimApplication.class, args);
 	}
 
 }
