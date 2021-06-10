15db60b2ad65ee5ef7479bed10b03dcce57d68e8
Morgan Taylor <morganxntaylor@gmail.com>
2021-06-10 11:12:07 -0700

    switch next and stream method responsibilities

--- src/test/java/com/dkkm/marketsim/model/dto/Mocker.java
+++ src/test/java/com/dkkm/marketsim/model/dto/Mocker.java
@@ -1,7 +1,5 @@
 package com.dkkm.marketsim.model.dto;
 
-import org.apache.tomcat.jni.Local;
-
 import java.time.LocalDate;
 import java.util.*;
 import java.util.stream.DoubleStream;
@@ -9,19 +7,6 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Mocker extends Random {
-
-    // data type generators
-    private Iterator<String> tickerIter;
-    private Iterator<LocalDate> dateIter;
-    private Iterator<Double> priceIter;
-    private Iterator<Integer> idIter;
-    private Iterator<Double> cashIter;
-
-    // DTO generators
-    private Iterator<Stock> stockIter;
-    private Iterator<Closing> closingIter;
-    private Iterator<Holding> holdingIter;
-    private Iterator<Portfolio> portfolioIter;
 
     public Mocker(long seed) {
         super(seed);
@@ -45,190 +30,142 @@ public class Mocker extends Random {
     }
 
     public Stream<Stock> stocks() {
-        Stream<Stock> stockStream = Stream.generate(() -> {
-            Stock stock = new Stock();
-            stock.setTicker(nextTicker());
-            stock.setIpo(nextDate());
-
-            return stock;
-        });
-        if (stockIter == null) {
-            stockIter = stockStream.iterator();
-        }
+        Stream<Stock> stockStream = Stream.generate(this::nextStock);
         return stockStream;
     }
 
     public Stock nextStock() {
-        return stockIter.next();
-    }
-
-    public Stream<Closing> closings(Iterator<Stock> stockIter) {
-        Stream<Closing> closingStream = Stream.generate(() -> {
-            Closing closing = new Closing();
-            closing.setDate(nextDate());
-            closing.setPrice(nextPrice());
-
-            // make valid connection
-            Stock stock = stockIter.next();
-            closing.setTicker(stock.getTicker());
-            closing.setStock(stock);
-
-            return closing;
-        });
-        if (closingIter == null) {
-            closingIter = closingStream.iterator();
-        }
-        return closingStream;
+        Stock stock = new Stock();
+        stock.setTicker(nextTicker());
+        stock.setIpo(nextDate());
+        return stock;
     }
 
     public Stream<Closing> closings() {
-        return closings(stockIter);
+        Stream<Closing> closingStream = Stream.generate(this::nextClosing);
+        return closingStream;
     }
 
     public Closing nextClosing() {
-        return closingIter.next();
-    }
+        Closing closing = new Closing();
+        closing.setDate(nextDate());
+        closing.setPrice(nextPrice());
 
-    public Stream<Holding> holdings(Iterator<Closing> closingIter, Iterator<Portfolio> portfolioIter) {
+        // make valid connection
+        Stock stock = nextStock();
+        closing.setTicker(stock.getTicker());
+        closing.setStock(stock);
 
-        Stream<Holding> holdingStream = Stream.generate(() -> {
-            Holding holding = new Holding();
-            holding.setPortfolioId(portfolioIter.next().getId());
-            holding.setShareQuantity(nextId());
-
-            // make valid connections
-            Closing closing = closingIter.next();
-            holding.setTicker(closing.getTicker());
-            holding.setPurchaseDate(closing.getDate());
-            holding.setClosing(closing);
-
-            return holding;
-        });
-        if (holdingIter == null) {
-            holdingIter = holdingStream.iterator();
-        }
-        return holdingStream;
+        return closing;
     }
 
     public Stream<Holding> holdings() {
-        return holdings(closingIter, portfolioIter);
+        Stream<Holding> holdingStream = Stream.generate(this::nextHolding);
+        return holdingStream;
     }
 
     public Holding nextHolding() {
-        return holdingIter.next();
+        Holding holding = new Holding();
+        holding.setPortfolioId(nextPortfolio().getId());
+        holding.setShareQuantity(nextId());
+
+        // make valid connections
+        Closing closing = nextClosing();
+        holding.setTicker(closing.getTicker());
+        holding.setPurchaseDate(closing.getDate());
+        holding.setClosing(closing);
+
+        return holding;
     }
 
     public Stream<Portfolio> portfolios() {
-        Stream<Portfolio> portfolioStream = Stream.generate(() -> {
-            Portfolio portfolio = new Portfolio();
-            portfolio.setId(nextId());
-
-            // make real valid holdings to insert into table
-            List<Holding> holdings = new ArrayList<>();
-            for (int i = 0; i < super.nextInt(12); i++) {
-                Holding holding = holdingIter.next();
-                holding.setPortfolioId(portfolio.getId());
-                holdings.add(holding);
-            }
-            portfolio.setHoldings(holdings);
-
-            portfolio.setCash(nextCash());
-            portfolio.setStartCash(nextCash());
-
-            // verify start date comes first
-            LocalDate startDate = nextDate();
-            LocalDate currentDate = nextDate();
-            if (currentDate.isBefore(startDate)) {
-                portfolio.setStartDate(currentDate);
-                portfolio.setDate(startDate);
-            } else {
-                portfolio.setStartDate(startDate);
-                portfolio.setDate(currentDate);
-            }
-
-            return portfolio;
-        });
-
-        if (portfolioIter == null) {
-            portfolioIter = portfolioStream.iterator();
-        }
-
+        Stream<Portfolio> portfolioStream = Stream.generate(this::nextPortfolio);
         return portfolioStream;
     }
 
     public Portfolio nextPortfolio() {
-        return portfolioIter.next();
+        Portfolio portfolio = new Portfolio();
+        portfolio.setId(nextId());
+
+        // make real valid holdings to insert into table
+        List<Holding> holdings = new ArrayList<>();
+        for (int i = 0; i < super.nextInt(12); i++) {
+            Holding holding = nextHolding();
+            holding.setPortfolioId(portfolio.getId());
+            holdings.add(holding);
+        }
+        portfolio.setHoldings(holdings);
+
+        portfolio.setCash(nextCash());
+        portfolio.setStartCash(nextCash());
+
+        // verify start date comes first
+        LocalDate startDate = nextDate();
+        LocalDate currentDate = nextDate();
+        if (currentDate.isBefore(startDate)) {
+            portfolio.setStartDate(currentDate);
+            portfolio.setDate(startDate);
+        } else {
+            portfolio.setStartDate(startDate);
+            portfolio.setDate(currentDate);
+        }
+
+        return portfolio;
     }
 
     public IntStream ids() {
         IntStream idStream = super.ints(0, Integer.MAX_VALUE);
-        if (idIter == null) {
-            idIter = idStream.iterator();
-        }
         return idStream;
     }
 
     public Integer nextId() {
-        return idIter.next();
+        return super.ints(0, Integer.MAX_VALUE).iterator().nextInt();
     }
 
     public DoubleStream prices() {
         DoubleStream doubleStream = super.doubles(0, 400000);
-        if (priceIter == null) {
-            priceIter = doubleStream.iterator();
-        }
         return doubleStream;
     }
 
     public Double nextPrice() {
-        return priceIter.next();
+        return super.doubles(0, 400000).iterator().next();
     }
 
     public DoubleStream cashes() {
         DoubleStream doubleStream = super.doubles(0, Double.MAX_VALUE);
-        if (cashIter == null) {
-            cashIter = doubleStream.iterator();
-        }
         return doubleStream;
     }
 
     public Double nextCash() {
-        return cashIter.next();
+        return super.doubles(0, Double.MAX_VALUE).iterator().next();
     }
 
     public Stream<String> tickers() {
-        int capA = 65; // ascii code for A
-        int capZ = 90; // ascii code for Z
-        PrimitiveIterator.OfInt chars = super.ints(capA, capZ).iterator();
-        Stream<String> tickerStream = Stream.generate(() -> {
-            StringBuilder builder = new StringBuilder();
-            for (int i = 0; i < 4 - super.nextInt(4); i++) {
-                builder.appendCodePoint(chars.nextInt());
-            }
-            return builder.toString();
-        });
-        if (tickerIter == null) {
-            tickerIter = tickerStream.iterator();
-        }
+        Stream<String> tickerStream = Stream.generate(this::nextTicker);
         return tickerStream;
     }
 
     public String nextTicker() {
-        return tickerIter.next();
+        int asciiMin = 65; // ascii code for A
+        int asciiMax = 90; // ascii code for Z
+        int tickerLength = 4 - super.nextInt(4);
+
+        // generates a stream of ints with ascii codepoints of capital letters, limits the length to (0,4]
+        // uses StringBuilder to turn the stream into a string.
+        return super.ints(asciiMin, asciiMax).limit(tickerLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
     }
 
     public Stream<LocalDate> dates() {
-        long epoch = LocalDate.EPOCH.toEpochDay();
-        long now = LocalDate.now().toEpochDay();
-        PrimitiveIterator.OfLong dayIter = super.longs(epoch, now).iterator();
-        Stream<LocalDate> dateStream = Stream.generate(() -> (LocalDate.ofEpochDay(dayIter.nextLong())));
-        if (dateIter == null) {
-            dateIter = dateStream.iterator();
-        }
+        Stream<LocalDate> dateStream = Stream.generate(this::nextDate);
         return dateStream;
     }
 
     public LocalDate nextDate() {
-        return dateIter.next();
+        long epoch = LocalDate.EPOCH.toEpochDay();
+        long now = LocalDate.now().toEpochDay();
+        long epochDay = super.longs(epoch, now).limit(1).iterator().nextLong();
+        return LocalDate.ofEpochDay(epochDay);
     }
 }
