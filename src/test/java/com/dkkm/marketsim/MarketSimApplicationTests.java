2afd59019697f746a79203b10e8d4f5ce70c500c
Morgan Taylor <morganxntaylor@gmail.com>
2021-06-10 10:11:29 -0700

    add junit4 platform runner and runner to allow for test suites

--- src/test/java/com/dkkm/marketsim/MarketSimApplicationTests.java
+++ src/test/java/com/dkkm/marketsim/MarketSimApplicationTests.java
@@ -1,13 +1,13 @@
 package com.dkkm.marketsim;
 
 import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@RunWith(JUnitPlatform.class)
+@SelectPackages({ "com.dkkm.marketsim" })
 class MarketSimApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
