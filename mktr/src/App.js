56667dec3f965c80ec03060221fdbd25456afad3
Morgan Taylor <morganxntaylor@gmail.com>
2021-06-16 09:05:14 -0700

    add grouped imports for views plus todo

--- mktr/src/App.js
+++ mktr/src/App.js
@@ -9,19 +9,18 @@ import {Container, Row, Col} from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-import APIService from './APIService'
-
-import GameView from './views/GameView'
-import StartView from './views/StartView'
-import LoaderView from './views/LoaderView';
+import {GameView, StartView, LoaderView} from './views/*';
 
 
 class App extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
+			// TODO: save the active game here so that in the event of clicking 
+			// out of the game it opens back up with minimal fuss.
+			// or at least the active game id?
 			activeGame: null,
-		}
+		};
 	}
 
 	redirectToLoaderView(portfolio) {
