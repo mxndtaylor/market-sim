07ccca36ddbd0217efe6425791928ed6a366f11a
Morgan Taylor <morganxntaylor@gmail.com>
2021-06-17 10:04:24 -0700

    edit endpoints so backend and frontend match

--- backend/src/main/java/com/dkkm/marketsim/controller/PortfolioController.java
+++ backend/src/main/java/com/dkkm/marketsim/controller/PortfolioController.java
@@ -18,70 +18,70 @@ import java.util.List;
 @RequestMapping("/api/portfolios")
 public class PortfolioController {
 
-    @Autowired
-    private PortfolioService portfolioService;
+	@Autowired
+	private PortfolioService portfolioService;
 
-    @GetMapping("")
-    @ResponseStatus(HttpStatus.OK)
-    public List<Portfolio> fetchPortfolioList() {
-        return portfolioService.getMembers();
-    }
+	@GetMapping("")
+	@ResponseStatus(HttpStatus.OK)
+	public List<Portfolio> fetchPortfolioList() {
+		return portfolioService.getMembers();
+	}
 
-    @PostMapping("/member")
-    public ResponseEntity<Portfolio> createPortfolio(@RequestBody Portfolio portfolio) {
-        // TODO: add validator check
-        portfolio = portfolioService.addMember(portfolio);
-        return new ResponseEntity<>(portfolio, HttpStatus.CREATED);
-    }
+	@PostMapping("/member")
+	public ResponseEntity<Portfolio> createPortfolio(@RequestBody Portfolio portfolio) {
+		// TODO: add validator check
+		portfolio = portfolioService.addMember(portfolio);
+		return new ResponseEntity<>(portfolio, HttpStatus.CREATED);
+	}
 
-    @GetMapping("/member/{memberId}")
-    public ResponseEntity<Portfolio> fetchPortfolio(@PathVariable int memberId) {
-        Portfolio portfolio = portfolioService.getMemberByKey(memberId);
-        // TODO: add validator check
-        return ResponseEntity.ok(portfolio);
-    }
+	@GetMapping("/member/{memberId}")
+	public ResponseEntity<Portfolio> fetchPortfolio(@PathVariable int memberId) {
+		Portfolio portfolio = portfolioService.getMemberByKey(memberId);
+		// TODO: add validator check
+		return ResponseEntity.ok(portfolio);
+	}
 
-    @PostMapping("/member/{memberId}/holdings/member")
-    public ResponseEntity<Integer> buyPortfolioShares(
-            @PathVariable int memberId,
-            @RequestBody Holding buyOrder) {
-        String ticker = buyOrder.getTicker();
-        LocalDate date = buyOrder.getPurchaseDate();
-        int buyQuantity = buyOrder.getShareQuantity();
-        Holding bought = portfolioService.buyTickerQuantityForPortfolio(memberId, ticker, date, buyQuantity);
-        return new ResponseEntity<>(bought.getShareQuantity(), HttpStatus.OK);
-    }
+	@PostMapping("/member/{memberId}/holdings/member")
+	public ResponseEntity<Integer> buyPortfolioShares(
+			@PathVariable int memberId,
+			@RequestBody Holding buyOrder) {
+		String ticker = buyOrder.getTicker();
+		LocalDate date = buyOrder.getPurchaseDate();
+		int buyQuantity = buyOrder.getShareQuantity();
+		Holding bought = portfolioService.buyTickerQuantityForPortfolio(memberId, ticker, date, buyQuantity);
+		return new ResponseEntity<>(bought.getShareQuantity(), HttpStatus.OK);
+	}
 
-    @PatchMapping("/member/{memberId}/holdings/member")
-    public ResponseEntity<Integer> sellPortfolioShares(
-            @PathVariable int memberId,
-            @RequestBody Holding sellOrder) {
-        // TODO: add validator check
-        int quantity = sellOrder.getShareQuantity();
-        String ticker = sellOrder.getTicker();
-        Holding sold = portfolioService.sellTickerQuantityFromPortfolio(memberId, ticker, quantity);
+	@PatchMapping("/member/{memberId}/holdings/member")
+	public ResponseEntity<Integer> sellPortfolioShares(
+			@PathVariable int memberId,
+			@RequestBody Holding sellOrder) {
+		// TODO: add validator check
+		int quantity = sellOrder.getShareQuantity();
+		String ticker = sellOrder.getTicker();
+		Holding sold = portfolioService.sellTickerQuantityFromPortfolio(memberId, ticker, quantity);
 
-        return new ResponseEntity<>(sold.getShareQuantity(), HttpStatus.OK);
-    }
+		return new ResponseEntity<>(sold.getShareQuantity(), HttpStatus.OK);
+	}
 
-    @PutMapping("/member")
-    public ResponseEntity updatePortfolio(@RequestBody Portfolio portfolio) {
-        boolean succeeded = portfolioService.updateMember(portfolio);
-        // TODO: add exception on false
-        return ResponseEntity.noContent().build();
-    }
+	@PutMapping("/member")
+	public ResponseEntity updatePortfolio(@RequestBody Portfolio portfolio) {
+		boolean succeeded = portfolioService.updateMember(portfolio);
+		// TODO: add exception on false
+		return ResponseEntity.noContent().build();
+	}
 
-    @PatchMapping("/member")
-    public ResponseEntity nextMarketDay(@RequestBody Portfolio portfolio) {
-        boolean succeeded = portfolioService.nextMarketDay(portfolio);
-        // TODO: add exception on false
-        return ResponseEntity.noContent().build();
-    }
+	@PatchMapping("/member")
+	public ResponseEntity nextMarketDay(@RequestBody Portfolio portfolio) {
+		boolean succeeded = portfolioService.nextMarketDay(portfolio);
+		// TODO: add exception on false
+		return ResponseEntity.noContent().build();
+	}
 
-    @DeleteMapping("/member/{memberId}")
-    public ResponseEntity deletePortfolio(@PathVariable int memberId) {
-        boolean succeeded = portfolioService.deleteMemberByKey(memberId);
-        // TODO: add exception on false
-        return ResponseEntity.noContent().build();
-    }
+	@DeleteMapping("/member/{memberId}")
+	public ResponseEntity deletePortfolio(@PathVariable int memberId) {
+		boolean succeeded = portfolioService.deleteMemberByKey(memberId);
+		// TODO: add exception on false
+		return ResponseEntity.noContent().build();
+	}
 }
--- mktr/src/APIService.js
+++ mktr/src/APIService.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 
-const GET_API_PORTFOLIO = "http://localhost:8080/api/portfolios/";
-const GET_API_STOCKS = "http://localhost:8080/api/tickers/"
-const GET_API_CLOSINGS = "http://localhost:8080/api/closings/"
+const GET_API_PORTFOLIO = "http://localhost:8080/api/portfolios";
+const GET_API_STOCKS = "http://localhost:8080/api/tickers"
+const GET_API_CLOSINGS = "http://localhost:8080/api/closings"
 
 // TODO: move API endpoints to a file that the backend can also access
 // have the front and back generate their endpoints from that file
 // manually set the endpoints in that file (maybe a RAML file? parsing would be an issue)
-// potentially in application.properties
+// potentially in application.properties, but react wouldn't find it in imports
+// potentially make a OPTIONS request to the api? I like this idea best so far...
 
 class APIService {
 
@@ -15,7 +16,7 @@ class APIService {
 	// ideally saved games have the relevant data in the table
 	// old, unused stuff should be purged somehow
 	initializeStocks(date) {
-		return axios.get(GET_API_STOCKS + 'initDB/' + date);
+		return axios.get(GET_API_STOCKS + '/initDB/' + date);
 	}
 
 	getMarketByDate(date) {
@@ -23,16 +24,15 @@ class APIService {
 	}
 
 	createPortfolio(date, principle) {
-		if (principle == null || principle == 0) {
+	    // true iff principle = null or 0
+		if (principle ?? 0 == 0) {
 			principle = 10000;
 		} else {
 			principle = Math.abs(principle);
 		}
 
 		const portfolio = {
-			date: date, 
-			cash: principle, 
-			startDate: date, 
+			startDate: date,
 			startCash: principle
 		};
 
@@ -42,11 +42,9 @@ class APIService {
 		);
 	}
 
-	nextDay(portfolio) {
-		portfolio.date.setDate(date.getDate() + 1);
-		return axios.put(
-			GET_API_PORTFOLIO + "/member", 
-			portfolio
+	fetchPortfolio(portfolioId) {
+		return axios.get(
+			GET_API_PORTFOLIO + "/member/" + portfolioId
 		);
 	}
 
@@ -66,9 +64,17 @@ class APIService {
 			ticker: ticker,
 			quantity: quantity,
 		};
-		return axios.put(
+		return axios.patch(
 			GET_API_PORTFOLIO + "/member/" + portfolioId + "/holdings/member", 
 			sellOrder
+		);
+	}
+
+	nextDay(portfolio) {
+		portfolio.date.setDate(date.getDate() + 1);
+		return axios.patch(
+			GET_API_PORTFOLIO + "/member",
+			portfolio
 		);
 	}
 
